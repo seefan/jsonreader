5f0b391e68b752d477dd46556781bc130ffd9868
seefan <seefan@126.com>
2019-03-31 18:38:09 +0800

    update test

--- jsonvalue_test.go
+++ jsonvalue_test.go
@@ -12,15 +12,15 @@ import (
 
 func TestJsonValue_GetValue(t *testing.T) {
 	r := ParseJsonObject([]byte(" {\"data\" : {\"key\" : 123,\"abc\":-1021e5 } , \"value\":5 ,\"ars\":[1,2,3,4,{\"value\":5},\"6\",{\"value\":7}]}"))
-	t.Log(r.GetJsonObject("data").GetJsonValue("abc").String())
+	t.Log(r.GetObject("data").GetValue("abc"))
 
-	r.GetJsonArray("ars").each(func(i int, value JsonValue) {
+	r.GetArray("ars").Each(func(i int, value JsonValue) {
 		t.Log(i, value)
 	})
 	arr := ParseJsonArray([]byte("[{\"data\" : {\"key\" : 123,\"abc\":-1021e5 } , \"value\":5 ,\"ars\":[1,2,3,4,{\"value\":5},\"6\",{\"value\":7}]},0,1,\"sfdada\"]"))
-	arr.each(func(i int, value JsonValue) {
+	arr.Each(func(i int, value JsonValue) {
 		if i == 0 {
-			t.Log(value.JsonObject().GetJsonObject("data").GetJsonValue("key"))
+			t.Log(value.ParseJsonObject().GetObject("data").GetValue("key"))
 		}
 		t.Log(i, value)
 	})
